71c7d5dbff32b5bcdab6339230441fc500cf81b4
Natzgun <itzlilouxx@gmail.com>
2025-05-24 12:48:21 -0500

    test

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,25 @@
+#include "AStar.h"
+#include "Graph.h"
+#include "Node.h"
 #include "Terrain.h"
-#include <iostream>
 #include <vector>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
   Terrain terrain(60, 30);
-  terrain.showTerrain();
+  // terrain.showTerrain();
+
+  Graph graph(terrain);
+  Node start{0, 0, 0};
+  Node goal{29, 14, 0};
+
+  vector<Node> path = AStar::findPath(graph, start, goal);
+
+  for (auto n : path) {
+    cout << "(" << n.x << "," << n.y << ") ";
+  }
+  cout << endl;
 
   return 0;
 }
